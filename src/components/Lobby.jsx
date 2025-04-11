e3724b207e1d7aac02d47734f4ba986b3881fc51
Sandeep Saini <sandeepsaini0204@gmail.com>
2025-04-11 21:00:59 +0530

    update lobby form position

--- src/components/Lobby.jsx
+++ src/components/Lobby.jsx
@@ -7,7 +7,7 @@ export const Lobby = ({ onEnterChat }) => {
       className="p-6 flex justify-center items-center min-h-screen"
       onSubmit={onEnterChat}
     >
-      <div className="flex flex-col gap-6 border rounded-xl shadow-md p-8 w-full max-w-md bg-white">
+    <div className="relative flex flex-col gap-6 border rounded-xl shadow-md p-8 w-full max-w-md bg-white -mt-40">
         <h1 className="text-2xl font-semibold text-center">Enter Username</h1>
         <TextInput
           id="username"
